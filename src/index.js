a56bea3395beec7dd4877250728234fec67b5fb2
akira-hiroshima <77510760+akira-hiroshima@users.noreply.github.com>
2021-03-14 18:44:47 +0300

    feat: end typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+	if (array && array.length){
+		let counter;
+	counter=array[0];
+	for (i=1;i<array.length;i++){
+		if (counter>array[i]) {counter=array[i]}
+	}
+	return counter;}
+	else {return 0;}
 }
 
 exports.max = function max (array) {
-  return 0;
+	if (array && array.length){
+	let counter;
+	counter=array[0];
+	for (i=1;i<array.length;i++){
+		if (counter<array[i]) {counter=array[i]}
+	}
+	return counter;}
+	else {  return 0;}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if (array && array.length){
+	let counter=0;
+	for(i=0;i<array.length;i++){
+	counter = counter + array[i]
+	}
+	counter = counter/array.length;
+	return counter;}
+	else {return 0;}
 }
